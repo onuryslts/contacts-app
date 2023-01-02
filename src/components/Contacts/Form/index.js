eee574e3ca0cc57aa0f3097ee99c5fb20186cfbb
Onur Yeşiltaş <40572534+onuryslts@users.noreply.github.com>
2023-01-02 15:43:11 +0300

    Add files via upload

---
+++ src/components/Contacts/Form/index.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+
+function Form({ addContact, contacts }) {
+  const [form, setForm] = useState({ fullname: "", phone_number: "" });
+
+  useEffect(() => {
+    setForm({ fullname: "", phone_number: "" });
+  }, [contacts]);
+
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (form.fullname === "" || form.phone_number === "") {
+      return false;
+    }
+    addContact([...contacts, form]);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          name="fullname"
+          placeholder="Fullname"
+          value={form.fullname}
+          onChange={onChangeInput}
+        />
+      </div>
+      <div>
+        <input
+          name="phone_number"
+          placeholder="Phone Number"
+          value={form.phone_number}
+          onChange={onChangeInput}
+        />
+      </div>
+      <div className="btn">
+      <button>Add</button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
